610490e9aab45ce33eb59770b4ee3f24a0f1f70e
noob_73 <69976635+Minhaj2441139@users.noreply.github.com>
2022-08-09 23:25:06 +0600

    Add files via upload

---
+++ FINAL1/src/termAndCondition.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+class Terms extends JFrame{
+    JLabel j;
+    Terms(){
+        setLayout(null);
+        setTitle("Terms and Conditions");
+
+    }
+}
+
+public class termAndCondition {
+    public static void main(String[] args) {
+
+
+    }
+
+}
